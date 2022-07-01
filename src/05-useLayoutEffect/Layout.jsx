673f6ea911e74fd48f57baaf8f506e9cf21d767f
XyonaX <zaxttpro@gmail.com>
2022-06-30 21:17:56 -0300

    Layout add

--- src/05-useLayoutEffect/Layout.jsx
+++ src/05-useLayoutEffect/Layout.jsx
@@ -18,23 +18,6 @@ export const Layout = () => {
         <h1 className="text-center">BreakingBad Quotes</h1>
         <hr />
 
-        {/* {
-            (isLoading) ? (
-
-                <div className="alert alert-info text-center">
-                Loading...
-                </div>
-            ) 
-            : (
-                <blockquote className="blockquote text-end">
-                    <p className="mb-1.1">{ quote}</p>
-                    <footer className="blockquote-footer">{author}</footer>
-                </blockquote>
-
-            )
-
-        } */}
-
         {
             isLoading ? <LoadingQuote/>
             : <Quote author={author} quote={quote}/>
